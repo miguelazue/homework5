b59d39a023ed868309159df445e79b593817959c
miguelazue <77974719+miguelazue@users.noreply.github.com>
2022-05-16 20:29:47 +0200

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -8,5 +8,22 @@ import streamlit as st
 import pandas as pd
 
 
-st.write("""Hello Motherfuckets""")
 
+with st.container():
+    st.subheader("Deploying my first app")
+    st.title("Forecast prediction")
+    st.write("""Hello Motherfuckets""")
+    
+    title = st.text_input('Write your name', 'Life of Brian')
+    MinTemp = st.text_input("MinTemp","")
+    MaxTemp = st.text_input("MaxTemp","")
+    Rainfall = st.text_input("Rainfall","")
+    Evaporation = st.text_input("Evaporation","")
+    Sunshine = st.text_input("Sunshine","")
+    WindGustSpeed = st.text_input("WindGustSpeed","")
+    Humidity9am = st.text_input("Humidity9am","")
+    Humidity3pm = st.text_input("Humidity3pm","")
+    Pressure9am = st.text_input("Pressure9am","")
+    Pressure3pm = st.text_input("Pressure3pm","")
+    Cloud3pm = st.text_input("Cloud3pm","")
+    Temp3pm = st.text_input("Temp3pm","")
